901fc534e0fc70acfce4eff168104333e708dbe4
e2247008@bit.uom.lk <e2247008@bit.uom.lk>
2025-06-08 11:30:36 +0530

    error fixed the barcode

--- dist/pages/inventoryManger/Inventorytable.php
+++ dist/pages/inventoryManger/Inventorytable.php
@@ -11,7 +11,7 @@ include('db_connect.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HHIMS - Add Items</title>
+    <title>HHIMS - Add Inventory Table</title>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
